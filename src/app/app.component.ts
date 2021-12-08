21b4d4c7e5e77b412e0e7076b3c61fb28590c676
moonlitangel <thfqlcdl@gmail.com>
2021-12-08 15:09:33 +0900

    slide release on edges

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 
+import SwiperCore, {Swiper, Mousewheel, Pagination, SwiperOptions} from 'swiper';
+SwiperCore.use([Mousewheel, Pagination]);
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'maple-blog';
+  testOption: SwiperOptions = {
+    direction: 'vertical',
+    speed: 1000,
+    touchReleaseOnEdges: true,
+    mousewheel: {
+      thresholdTime: 1000,
+      releaseOnEdges: true
+    }
+  }
+  constructor() {
+  }
+
 }
